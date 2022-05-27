167ad9c02208f372960c6bded81a92bcf9e41cab
Ícaro Bruno <54824248+icarob-eng@users.noreply.github.com>
2022-05-27 16:53:03 -0300

    leitor de JSON finalizado

--- app/src/main/java/com/example/clima/MainActivity.java
+++ app/src/main/java/com/example/clima/MainActivity.java
@@ -5,20 +5,21 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Calendar;
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
     private CardView natalCard;
     private TextView textView;
+    private final int dia = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+
+    private final String url="https://api.open-meteo.com/v1/forecast?latitude=-5.79&longitude=-35.34&daily=temperature_2m_max&timezone=America%2FSao_Paulo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +27,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         natalCard = findViewById(R.id.natalCard);
-
         textView = findViewById(R.id.natalNome);
+        textView.setText(WeatherClient.getTemperature("Natal"));
 
-        natalCard.setOnClickListener(v -> textView.setText(request(
-                "https://api.open-meteo.com/v1/forecast?latitude=-5.79&longitude=-35.34&daily=temperature_2m_max&timezone=America%2FSao_Paulo"))
-//                startActivity(
-//                new Intent(MainActivity.this, detalhesClima.class)
-        );
-
+        natalCard.setOnClickListener(v -> startActivity(
+                new Intent(MainActivity.this, detalhesClima.class)
+        ));
 
     }
 }
--- app/src/main/java/com/example/clima/WeatherClient.java
+++ app/src/main/java/com/example/clima/WeatherClient.java
@@ -1,5 +1,11 @@
 package com.example.clima;
 
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,34 +13,79 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Objects;
 
 public class WeatherClient {
-    public String request(String link){
-        String response = "";
-        try {
-            URL url = new URL(link);
-            HttpURLConnection urlConnection = null;
+    private static volatile String response = "";
+    private static final HashMap<String, double[]> coordenadas = new HashMap<String, double[]>() {{
+        put("Natal", new double[]{-5.79, -35.34});
+        put("Helsinki", new double[]{60.17, 24.94});
+        put("Nova Iorque", new double[]{40.72, -73.99});
+        put("Bova Delhi", new double[]{28.52,77.06});
+    }};
+
+    public static void request(String link){
+        AsyncTask.execute(() -> {
             try {
-                urlConnection = (HttpURLConnection) url.openConnection();
-                StringBuffer buffer = new StringBuffer();
-                InputStream is = urlConnection.getInputStream();
-                BufferedReader br = new BufferedReader(new InputStreamReader(is));
-                String line = null;
-                while ((line = br.readLine()) != null)
-                    buffer.append(line);
-                is.close();
-                urlConnection.disconnect();
-                response = buffer.toString();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (urlConnection != null) {
+                URL url = new URL(link);
+                HttpURLConnection urlConnection = null;
+                try {
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    StringBuffer buffer = new StringBuffer();
+                    InputStream is = urlConnection.getInputStream();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                    String line = null;
+                    while ((line = br.readLine()) != null)
+                        buffer.append(line);
+                    is.close();
                     urlConnection.disconnect();
+                    response = buffer.toString();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (urlConnection != null) {
+                        urlConnection.disconnect();
+                    }
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public static String getResponse(String link) {
+        request(link);
+        return response;
+    }
+
+    public static String getCityResponse(String cidade){
+        return getResponse(buildLink(cidade));
+    }
+
+    public static String buildLink(String cidade){
+        return "https://api.open-meteo.com/v1/forecast?latitude=" +
+                Objects.requireNonNull(coordenadas.get(cidade))[0] +
+                "&longitude=" +
+                Objects.requireNonNull(coordenadas.get(cidade))[0] +
+                "&daily=temperature_2m_max&timezone=America%2FSao_Paulo";
+    }
+
+    public static String getTemperature(String cidade) {
+        try {
+            String json = WeatherClient.getCityResponse(cidade);
+            JSONObject clima = new JSONObject(json);
+            for (Iterator<String> it = clima.keys(); it.hasNext(); ) {
+                String key = it.next();
+                boolean teste=key.equals("daily");
+                if (teste) {
+                    JSONObject daily=(JSONObject) clima.get(key);
+                    JSONArray temperatureSemana=daily.getJSONArray("temperature_2m_max");
+                    return temperatureSemana.get(dia).toString();
                 }
             }
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        return response;
+        } catch (JSONException ignored){}
+        return null;
     }
 }
