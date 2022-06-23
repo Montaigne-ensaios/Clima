4cdeccd0798a3829cd7e7d80215bd665b9363562
Ícaro Bruno <54824248+icarob-eng@users.noreply.github.com>
2022-06-23 20:23:18 -0300

    Cores por temperatura

--- app/src/main/java/com/example/clima/DetalhesClimaActivity.java
+++ app/src/main/java/com/example/clima/DetalhesClimaActivity.java
@@ -1,6 +1,8 @@
 package com.example.clima;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,6 +19,20 @@ public class DetalhesClimaActivity extends AppCompatActivity {
         Intent originIntent = getIntent();
         String cidade = originIntent.getStringExtra("cidade");
         String temperatura = originIntent.getStringExtra("temperatura");
+
+        ConstraintLayout fundo = findViewById(R.id.constraintLayout);
+        int temp = Integer.parseInt(temperatura.replace("°C", ""));
+        if(temp > 40){
+            fundo.setBackgroundColor(ContextCompat.getColor(this, R.color.muito_quente));
+        } else if (temp > 30){
+            fundo.setBackgroundColor(ContextCompat.getColor(this, R.color.quente));
+        } else if (temp > 20){
+            fundo.setBackgroundColor(ContextCompat.getColor(this, R.color.agradavel));
+        } else if (temp > 10){
+            fundo.setBackgroundColor(ContextCompat.getColor(this, R.color.frio));
+        } else if (temp < 10){
+            fundo.setBackgroundColor(ContextCompat.getColor(this, R.color.muito_frio));
+        }
 
         // atribui os valores às views
         TextView cidadeView = findViewById(R.id.cidade);
